932593f2688f18145cf38cb2c2c7fee94748753b
BrianMsane <msanebrianboss@gmail.com>
2025-12-25 10:07:00 +0200

    add: task three

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from quantium-starter-repo!")
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash()
 
 
-if __name__ == "__main__":
-    main()
+df = pd.read_csv('./data/clean_data.csv')
+df.index = df['date'] # sort by date
+
+
+fig = px.line(
+    data_frame=df,
+    x='date',
+    y='sales'
+)
+
+
+app.layout = html.Div(children=[
+    html.H1(
+        children='Pink Morsel Sales', 
+        style={'textAlign': 'center'}
+    ),
+    dcc.Graph(id='example-graph', figure=fig),
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
